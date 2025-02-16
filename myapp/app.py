286ec89055d4bfbb6ae44efe5fda33f330eb7f57
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-16 15:13:52 +0300

    add init method in class Sorter

--- myapp/app.py
+++ myapp/app.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import json
 
 app = Flask(__name__)
+
+class Sorter:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = None
