480bdf3a1566d44c8066411e618d8b99efeeee6a
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-02-16 15:15:03 +0300

    add read method in class sorter public, with raise errors

--- myapp/app.py
+++ myapp/app.py
@@ -7,3 +7,13 @@ class Sorter:
     def __init__(self, file_path):
         self.file_path = file_path
         self.data = None
+
+    def read(self):
+        try:
+            with open(self.file_path, "r") as f:
+                self.data = json.load(f)
+                if not isinstance(self.data, list):
+                    raise ValueError("Формат файла не соответствует ожидаемому. Ожидается список.")
+            return True
+        except Exception as e:
+            return {"error": f"Ошибка при чтении файла: {e}"}
